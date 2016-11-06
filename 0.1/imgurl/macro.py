992a87463fa3fe8163eb117764fb333da0bb66f0
bitelxux <bitelxux@gmail.com>
2016-11-06 14:04:53 +0000

    First code

---
+++ 0.1/imgurl/macro.py
@@ -0,0 +1,41 @@
+"""Img URL macro"""
+
+from genshi.builder import tag
+from trac.util.translation import cleandoc_
+from trac.wiki.macros import WikiMacroBase
+from trac.wiki.api import parse_args
+import requests
+import json
+import random
+
+
+class ImgURLMacro(WikiMacroBase):
+    _description = cleandoc_(
+    """Image from URL macro.
+
+    [[ImgURL(url[, width=<width>, height=<height>])]]
+
+    """)
+
+    def expand_macro(self, formatter, name, args):
+        """Returns an image that will be displayed in the Wiki content.
+
+        `name` is the actual name of the macro (no surprise, here it'll be
+        `'HelloWorld'`),
+        `args` is the text enclosed in parenthesis at the call of the
+          macro.
+        """
+
+        params = {}
+        args_list, args_dict = parse_args(args)
+
+        img = args_list[0]
+        params['eight'] = args_dict.get('eight', '')
+        params['width'] = args_dict.get('width', '')
+
+        return tag.image(src=img,
+                         width=params['width'],
+                         eight=params['eight'])
+
+    # Note that there's no need to HTML escape the returned data,
+    # as the template engine (Genshi) will do it for us.
---
+++ 0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+from setuptools import setup
+
+setup(
+    name='ImgURLMacro',
+    version='0.1',
+    packages=['imgurl'],
+    author="Bitelxux",
+    maintainer="Bitelxux",
+    maintainer_email="bitelxux@gmail.com",
+    description="Inline an image from an URL",
+    long_description="""Inline an image from an URL
+                     """,
+    license="GPL",
+    keywords="trac plugin image url",
+    url="",
+    entry_points={
+        'trac.plugins': [
+            'imgurl.macro = imgurl.macro'
+        ]
+    },
+)
